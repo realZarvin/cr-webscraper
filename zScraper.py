b7da319dc0818f35149188f04b7cfdee9510687d
Wali Chizarum <155149253+realZarvin@users.noreply.github.com>
2024-07-29 23:45:33 +0100

    Update zScraper.py

--- zScraper.py
+++ zScraper.py
@@ -1,14 +1,18 @@
+#Importing all Libraries;
 import requests
 from bs4 import BeautifulSoup
 import time
+
 
 # Fetch the HTML content of the main page
 url = 'https://cryptorank.io'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Find all anchor tags with the specified class
+
+# Find all anchor tags with the specified class, which I no understand.
 elements = soup.find_all("a", class_="sc-39ba2409-1 bgOSTi")
+
 
 # Collecting the links from the elements
 https_links = []
@@ -17,8 +21,10 @@ for element in elements:
     if link and link.startswith("/ico/"):
         https_links.append("https://cryptorank.io" + link)
 
+
 # List to store all project links
 main_links = []
+
 
 # Iterate through each ICO link
 for link in https_links:
@@ -26,8 +32,10 @@ for link in https_links:
     ico_response = requests.get(link)
     ico_soup = BeautifulSoup(ico_response.content, "html.parser")
     
+    
     # Find the div with the specified class
     div = ico_soup.find("div", class_="sc-7145b3a-0 sc-e160646e-1 ehuebT cQPrWc sc-fb0fcd25-0 dzRVol")
+    
     
     # Get the personal project links
     if div:
@@ -36,9 +44,11 @@ for link in https_links:
         if anchor:
             # Append the full URL to the main_links list
             main_links.append(f"https://cryptorank.io{anchor.get('href')}")
+            
     
     # Wait for 1 second before the next request
-    time.sleep(1)
+    time.sleep(0.87)
+
 
 # Print all collected project links
 for main_link in main_links:
